51002f933043f743b715dc89e5c089a6a9be5251
thefutureissoyesterday <logan.savinkoff@gmail.com>
2024-11-30 15:48:34 -0700

    UH CODE

--- uselesscode.php
+++ uselesscode.php
@@ -1,7 +1,10 @@
 <Head>
+   <link rel="stylesheet" type="text/css" href="https://blizzardcss.ct8.pl/blizzardbutton.css" />
 <title> Why? </title>
-<style> body{background-color: red;} </style>
+<style> body{background-color: red;} 
+</style>
 </Head>
 <body>
 <h1> Why did you decide to do this</h1>
+  <button> Im cool and useless</button>
 </body>
